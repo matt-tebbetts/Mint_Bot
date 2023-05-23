d362798303a04e5ba0bc863eda1a76bdd5e15bfb
Matt Tebbetts <matttebbetts@gmail.com>
2023-05-23 12:03:34 -0400

    confirmed column selection

--- mint.py
+++ mint.py
@@ -19,6 +19,10 @@ MINT_USER = os.getenv('MINT_USER')
 MINT_PASS = os.getenv('MINT_PASS')
 MINT_TOKEN = os.getenv('MINT_TOKEN')
 
+# get time
+now = datetime.now(pytz.timezone('US/Eastern')).strftime('%Y-%m-%d %H:%M:%S')
+now_str = datetime.now(pytz.timezone('US/Eastern')).strftime('%Y%m%d_%H%M%S')
+
 # connect
 def connect_to_mint():
     mint = Mint(MINT_USER,
@@ -36,6 +40,13 @@ def get_transactions(mint, now):
     # get the raw data
     trans_raw = mint.get_transaction_data()
 
+    # pick specific columns and keep in this order
+    cols_to_keep = ['id', 'date', 'amount', 'transactionType', 'description'
+        'category_name', 'category_parentName', 'accountRef_name', 'tagData_name'
+        'notes', 'isReviewed', 'lastUpdatedDate', 'parentId'
+        'fiData_amount', 'fiData_date', 'fiData_description'
+        'accountRef_id', 'accountRef_type', 'category_id', 'category_parentId', 'insert_ts']
+
     # removes inner level "tags" from "tagData" column
     for transaction in trans_raw:
         if 'tagData' in transaction:
@@ -47,11 +58,10 @@ def get_transactions(mint, now):
     trans_json = json_normalize(trans_raw)
     df = pd.DataFrame(trans_json)
 
-    columns_to_drop = df.columns[df.columns.str.contains('meta')]
-    df = df.drop(columns_to_drop, axis=1)
-
-    df['insert_ts'] = now
+    # confirm columns
     df.columns = df.columns.str.replace('[.\s]', '_', regex=True)
+    df['insert_ts'] = now
+    df = df[cols_to_keep]
 
     return df
 
@@ -76,32 +86,23 @@ def get_accounts(mint, now):
 
     return df
 
-# get time
-now = datetime.now(pytz.timezone('US/Eastern')).strftime('%Y-%m-%d %H:%M:%S')
-now_str = datetime.now(pytz.timezone('US/Eastern')).strftime('%Y%m%d_%H%M%S')
-
-# run
+# connect
 mint = connect_to_mint()
-trans_df = get_transactions(mint, now)
-accnt_df = get_accounts(mint, now)
-mint.close()
-
-# send to csv
-trans_df.to_csv(f'files/trans_df_{now_str}.csv', index=False)
-accnt_df.to_csv(f'files/accnt_df_{now_str}.csv', index=False)
-
-# send to sql
 engine = create_engine(sql_addr)
+
+# get transactions
+trans_df = get_transactions(mint, now)
+trans_df.to_csv(f'files/trans_df_{now_str}.csv', index=False)
 try:
     # get columns from table
-    trans_columns = pd.read_sql_query("SELECT * FROM transactions_history LIMIT 0", engine).columns
-    trans_filtered = trans_df[trans_columns]
-    trans_filtered.to_sql('transactions_history', con=engine, if_exists='append', index=False)
+    trans_df.to_sql('transactions_history', con=engine, if_exists='append', index=False)
     print(f"Success sending transactions to sql")
 except Exception as e:
     print(f"Error when trying to send transactions to sql: {e}")
 
-# send accounts to sql
+# get accounts
+accnt_df = get_accounts(mint, now)
+accnt_df.to_csv(f'files/accnt_df_{now_str}.csv', index=False)
 try:    
     accnt_columns = pd.read_sql_query("SELECT * FROM accounts_history LIMIT 0", engine).columns
     accnt_filtered = accnt_df[accnt_columns]
@@ -109,3 +110,6 @@ try:
     print(f"Success sending accounts to sql")
 except Exception as e:
     print(f"Error when trying to send accounts to sql: {e}")
+
+# close connection
+mint.close()
