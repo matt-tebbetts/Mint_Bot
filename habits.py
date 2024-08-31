4f1f96b5a8c7be34088f1d83a925f34729afd6a3
Matt Tebbetts <matttebbetts@gmail.com>
2024-08-31 13:40:09 -0400

    fixed up habits

--- habits.py
+++ habits.py
@@ -19,6 +19,24 @@ df = (
     .melt(id_vars='Habit', var_name='Date', value_name='Value')
 )
 
+# Define the habit replacements and their respective dates
+habit_replacements = {
+    'Screen Time old': {'new_habit': 'No Phone', 'date': '2023-10-16'},
+    'Vaping old': {'new_habit': 'No Smoking', 'date': '2023-10-15'}
+}
+
+# Apply the habit replacements and updates
+for old_habit, details in habit_replacements.items():
+    new_habit, date = details['new_habit'], details['date']
+
+    # Update 'Habit' and 'Value' for dates before the specified date
+    condition_before_date = (df['Habit'] == old_habit) & (df['Date'] < date)
+    df.loc[condition_before_date, 'Habit'] = new_habit
+    df.loc[condition_before_date & (df['Value'].isin([0, 2])), 'Value'] = df['Value'].map({2: 0, 0: 2})
+
+    # Remove rows where the old habit is present for dates on or after the specified date
+    df = df[~((df['Habit'] == old_habit) & (df['Date'] >= date))]
+
 # Load value_xref.csv and merge to translate 'Value' to 'Completed'
 df['Value'] = df['Value'].astype(float)
 value_xref = pd.read_csv(f'{my_dir}/value_xref.csv')
